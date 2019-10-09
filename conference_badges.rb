7aac72459c81133ace963fd9ee456316e29d51a4
Jeff Swisher <jefftswisher@gmail.com>
2019-10-09 17:15:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,11 +15,11 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-   batch_badge_creator(attendees) do |badge|
+   batch_badge_creator(attendees).each do |badge|
      puts badge
     end
 
-    assign_rooms(attendees) do |rooms|
+    assign_rooms(attendees).each do |rooms|
       puts rooms
     end
   end 
